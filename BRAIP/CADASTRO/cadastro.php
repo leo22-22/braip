597210b431a16cd0ecd979e860776266d4846bac
LEONARDO PICOLO DOS SANTOS DOS RANUCI <leonardoranuci17@gmail.com>
2025-01-28 20:10:45 -0300

    Add files via upload

--- BRAIP/CADASTRO/cadastro.php
+++ BRAIP/CADASTRO/cadastro.php
@@ -1,21 +1,89 @@
+<!-- filepath: /c:/xampp/htdocs/BRAIP/CADASTRO/cadastro.php -->
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CADASTRO CLIENTE</title>
+    <title>Cadastro</title>
+    <link rel="stylesheet" href="cadastro.css">
 </head>
 <body>
-<?php    
-$conexao = mysqli_connect('localhost','root','','db_cliente');
+    <header>
+        <h1 id="header">AFILIADO DE SUCESSO</h1>
+        <a href="../braip-main/index.html"><h5 id="header1">Voltar Início</h5></a>
+    </header>
+    <!-- Menu Hambúrguer -->
+    <div class="hamburguer" onclick="toggleMenu()">
+        <div class="bar"></div>
+        <div class="bar"></div>
+        <div class="bar"></div>
+    </div>
 
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$cpf_cnpj = $_POST['cpf_cnpj'];
+    <nav id="menu" class="menu-opcoes">
+        <ul>
+            <li><a href="../PRODUTOS/produtos.html">Produtos</a></li>
+            <li><hr></li>
+            <li><a href="http://localhost/BRAIP/CAMISAS/camisas.php">Camisas</a></li>
+            <li><hr></li>
+            <li><a href="../SERVICOS/servicos.html">Serviços</a></li>
+            <li><hr></li>
+            <li><a href="http://localhost/BRAIP/CADASTRO/cadastro.php">Bora Trabalhar Juntos!</a></li>
+        </ul>
+    </nav>
 
-$sql = "INSERT INTO dbl_cliente(nome,email,cpf_cnpj) VALUES ('$nome','$email','$cpf_cnpj')";
+    <main>
+        <h2>Cadastro</h2>
+        <form action="processa_cadastro.php" method="post" onsubmit="return validateForm()">
+            <div class="form-group">
+                <label for="nome">Nome:</label>
+                <input type="text" id="nome" name="nome" placeholder="Digite seu nome" required>
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input type="email" id="email" name="email" placeholder="Digite seu email" required>
+            </div>
+            <div class="form-group">
+                <label for="cpf_cnpj">CPF/CNPJ:</label>
+                <input type="text" id="cpf_cnpj" name="cpf_cnpj" placeholder="Digite seu CPF ou CNPJ" required>
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Cadastrar">
+            </div>
+        </form>
 
-$retorno = mysqli_query($conexao,$sql);
-?>
+        <p id="error-message" style="color: red;"></p> 
+    </main>
+
+    <footer>
+        <div id="contato">
+            <h1>Contato</h1>
+            <p>Telefone: <a class="links" href="https://wa.link/6pcon3">(18) 98111-3802</a></p>
+            <p>Email: <a class="links" href="mailto:leonardoranuci17@gmail.com?subject=leonardoranuci17@gmail.com&body=leonardoranuci17@gmail.com">leonardoranuci17@gmail.com</a></p>
+            <p>Instagram: <a class="links" href="https://instagram.com/seuusuario">@seuusuario</a></p>
+        </div>
+        
+        <div id="politica">
+            <h1>Política</h1>
+            <p><a class="links" href="politica.html">Política de Privacidade</a></p>
+        </div>
+    
+        <div id="empresa">
+            <h1>Empresa</h1>
+            <p>Sobre nós <br> Empresa de e-commerce apenas, com foco na venda de produtos <br> e na melhor satisfação possível do cliente.</p>
+            <p>Endereço</p>
+        </div>
+    </footer>
+    <script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/671a49712480f5b4f5933514/1iavatp6n';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <script src="cadastro.js"></script>
 </body>
 </html>
--- BRAIP/braip-main/index.js
+++ BRAIP/braip-main/index.js
@@ -18,3 +18,22 @@ function toggleMenu() {
     hamburger.classList.toggle('active');
 }
 
+//carrossel
+let currentSlide = 0;
+        const slides = document.querySelectorAll('.carousel-item');
+
+        function showSlide(index) {
+            slides.forEach((slide, i) => {
+                slide.classList.toggle('active', i === index);
+            });
+        }
+
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % slides.length;
+            showSlide(currentSlide);
+        }
+
+        function prevSlide() {
+            currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+            showSlide(currentSlide);
+        }
